a29fddf1850a46e0712250ad9168dd906c308049
harshic <harshireddy95@gmail.com>
2018-12-06 10:44:37 +0530

    date format changes in instrument program

--- Node/HTML/js/tc-admin.js
+++ Node/HTML/js/tc-admin.js
@@ -1109,3 +1109,14 @@ $("#messages").html('');
 return '<p class="message">'+_msg+'</p>';
 
 }
+function formatDate() {
+    var d = new Date(),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [day, month, year].join('/');
+}
